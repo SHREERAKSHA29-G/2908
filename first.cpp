1ac7bc71d52166491d7b8a5e76b2e9ad50a073f7
Shree Raksha <g.raksha@s.amity.edu>
2025-05-21 21:51:04 +0530

    1st commit on first file

---
+++ first.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x=10;float y=98.6;double z=123.456;bool a=true;
+    cout<<"welcome to the c++ world!!!"<<endl
+    <<"x is:"<<x<<endl 
+    <<"y is:"<<y<<endl
+    <<"z is:"<<z<<endl
+    <<"a is:"<<a<<endl;
+
+    return 0;
+}
