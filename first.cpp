f4250cbcecd3718193ad43f4e20d2cbbd255b4c3
Shree Raksha <g.raksha@s.amity.edu>
2025-05-21 21:53:27 +0530

    2nd commit on first file

--- first.cpp
+++ first.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 int main()
 {
-    int x=10;float y=98.6;double z=123.456;bool a=true;
-    cout<<"welcome to the c++ world!!!"<<endl
+    int x=10;float y=98.6;double z=123.456;bool a=false;
+    cout<<"welcome to Amity University Bengaluru!!!"<<endl
     <<"x is:"<<x<<endl 
     <<"y is:"<<y<<endl
     <<"z is:"<<z<<endl
